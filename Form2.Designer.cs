7c8cfd9c570ef7f234cea94516236aeb9f9192cd
unknown <yjyoon.dev@gmail.com>
2021-02-22 02:09:37 +0900

    즐겨찾기 리스트 구현

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -28,8 +28,147 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.add = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.no = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.code = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sub = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.point = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.prof = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.time = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.add,
+            this.no,
+            this.code,
+            this.sub,
+            this.point,
+            this.prof,
+            this.time});
+            this.dataGridView1.Location = new System.Drawing.Point(811, 379);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowHeadersVisible = false;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowTemplate.Height = 35;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(592, 319);
+            this.dataGridView1.StandardTab = true;
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // add
+            // 
+            this.add.HeaderText = "";
+            this.add.Name = "add";
+            this.add.Width = 50;
+            // 
+            // no
+            // 
+            this.no.HeaderText = "순번";
+            this.no.Name = "no";
+            this.no.Width = 37;
+            // 
+            // code
+            // 
+            this.code.HeaderText = "학정번호";
+            this.code.Name = "code";
+            this.code.Width = 110;
+            // 
+            // sub
+            // 
+            this.sub.HeaderText = "과목명";
+            this.sub.Name = "sub";
+            this.sub.Width = 200;
+            // 
+            // point
+            // 
+            this.point.HeaderText = "학점";
+            this.point.Name = "point";
+            this.point.Width = 37;
+            // 
+            // prof
+            // 
+            this.prof.HeaderText = "담당교수";
+            this.prof.Name = "prof";
+            this.prof.Width = 80;
+            // 
+            // time
+            // 
+            this.time.HeaderText = "강의시간";
+            this.time.Name = "time";
+            this.time.Width = 80;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 597);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(74, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "종료";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1359, 349);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(42, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "삭제";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("굴림", 9F);
+            this.button3.Location = new System.Drawing.Point(599, 599);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(77, 36);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "수강삭제";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Yellow;
+            this.button4.Font = new System.Drawing.Font("굴림", 9F);
+            this.button4.Location = new System.Drawing.Point(674, 599);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 36);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "수강신청";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // Form2
             // 
@@ -39,14 +178,34 @@
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.ClientSize = new System.Drawing.Size(1415, 724);
             this.ControlBox = false;
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
             this.DoubleBuffered = true;
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "광운대학교 수강신청 연습 프로그램";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewButtonColumn add;
+        private System.Windows.Forms.DataGridViewTextBoxColumn no;
+        private System.Windows.Forms.DataGridViewTextBoxColumn code;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sub;
+        private System.Windows.Forms.DataGridViewTextBoxColumn point;
+        private System.Windows.Forms.DataGridViewTextBoxColumn prof;
+        private System.Windows.Forms.DataGridViewTextBoxColumn time;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -15,6 +15,29 @@ namespace kw_enrolment_practice
         public Form2()
         {
             InitializeComponent();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.Rows.Add("조회","1", "0000-0-0000-00", "과목1", "3", "교수님", "월(1)");
+            dataGridView1.Rows.Add("조회", "2", "0000-0-0000-00", "과목2", "3", "교수님", "화(2)");
+            dataGridView1.Rows.Add("조회", "3", "0000-0-0000-00", "과목3", "3", "교수님", "수(3)");
+            dataGridView1.Rows.Add("조회", "4", "0000-0-0000-00", "과목4", "3", "교수님", "목(4)");
+            dataGridView1.Rows.Add("조회", "5", "0000-0-0000-00", "과목5", "3", "교수님", "금(5)");
+            dataGridView1.Rows.Add("조회", "6", "0000-0-0000-00", "과목6", "3", "교수님", "토(6)");
+            dataGridView1.Rows.Add("조회", "7", "0000-0-0000-00", "과목7", "3", "교수님", "일(7)");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
